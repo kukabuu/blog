78104b0c1d39dac600fdcb70d916b08e8e25a9ff
Ekaterina Blinova <kukabuu@gmail.com>
2019-05-31 16:39:00 +0300

    fix bugs

---
+++ dev/js/post.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+
+$(function() {
+	var editor = new MediumEditor("#post-body", {
+		placeholder: {
+			text: "",
+			hideOnClick: true
+		}
+	});
+
+	//remove errors
+	function removeErrors() {
+		$(".post-form p.error").remove();
+		$(".post-form input, #post-body").removeClass("error");
+	}
+
+	//clear
+	$(".post-form input, #post-body").on("focus", function() {
+		removeErrors();
+	});
+
+	//publish
+	$(".publish-button").on("click", function(e) {
+		e.preventDefault();
+		removeErrors();
+
+		var data = {
+			title: $("#post-title").val(),
+			body: $("#post-body").html()
+		};
+
+		$.ajax({
+			type: "POST",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			url: "/post/add"
+		}).done(function(data) {
+			console.log(data);
+			if (!data.ok) {
+				$(".post-form h2").after('<p class="error">' + data.error + "</p>");
+				if (data.fields) {
+					data.fields.forEach(function(item) {
+						$("#post-" + item).addClass("error");
+					});
+				}
+			} else {
+				$(location).attr("href", "/");
+			}
+		});
+	});
+});
+/* eslint-enable no-undef */
