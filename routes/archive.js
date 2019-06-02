ab17754b1b2d297981b6f827a81fbec7f3a34413
Ekaterina Blinova <kukabuu@gmail.com>
2019-06-02 23:26:03 +0300

    add user and post pages

--- routes/archive.js
+++ routes/archive.js
@@ -13,10 +13,12 @@ function posts(req, res) {
 	models.Post.find({})
 		.skip(perPage * page - perPage)
 		.limit(perPage)
+		.populate("owner")
+		.sort({ createdAt: -1 })
 		.then(posts => {
-			models.Post.estimatedDocumentCount()
+			models.Post.count()
 				.then(count => {
-					res.render("index", {
+					res.render("archive/index", {
 						posts,
 						current: page,
 						pages: Math.ceil(count / perPage),
@@ -26,13 +28,90 @@ function posts(req, res) {
 						}
 					});
 				})
-				.catch(console.log);
+				.catch(() => {
+					throw new Error("Server Error");
+				});
 		})
-		.catch(console.log);
+		.catch(() => {
+			throw new Error("Server Error");
+		});
 }
 
 //routers
 router.get("/", (req, res) => posts(req, res));
 router.get("/archive/:page", (req, res) => posts(req, res));
+
+router.get("/posts/:post", (req, res, next) => {
+	const url = req.params.post.trim().replace(/ +(?= )/g, "");
+	const userId = req.session.userId;
+	const userLogin = req.session.userLogin;
+
+	if (!url) {
+		const err = new Error("Not Found");
+		err.status = 404;
+		next(err);
+	} else {
+		models.Post.findOne({
+			url
+		}).then(post => {
+			if (!post) {
+				const err = new Error("Not Found");
+				err.status = 404;
+				next(err);
+			} else {
+				res.render("post/post", {
+					post,
+					user: {
+						id: userId,
+						login: userLogin
+					}
+				});
+			}
+		});
+	}
+});
+
+// users posts
+router.get("/users/:login/:page*?", (req, res) => {
+	const userId = req.session.userId;
+	const userLogin = req.session.userLogin;
+	const perPage = +config.PER_PAGE;
+	const page = req.params.page || 1;
+	const login = req.params.login;
+
+	models.User.findOne({
+		login
+	}).then(user => {
+		models.Post.find({
+			owner: user.id
+		})
+			.skip(perPage * page - perPage)
+			.limit(perPage)
+			.sort({ createdAt: -1 })
+			.then(posts => {
+				models.Post.count({
+					owner: user.id
+				})
+					.then(count => {
+						res.render("archive/user", {
+							posts,
+							_user: user,
+							current: page,
+							pages: Math.ceil(count / perPage),
+							user: {
+								id: userId,
+								login: userLogin
+							}
+						});
+					})
+					.catch(() => {
+						throw new Error("Server Error");
+					});
+			})
+			.catch(() => {
+				throw new Error("Server Error");
+			});
+	});
+});
 
 module.exports = router;
