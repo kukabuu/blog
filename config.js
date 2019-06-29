9f79ced68bb2613c47a0d92a169628be7b2de45b
Ekaterina Blinova <kukabuu@gmail.com>
2019-06-29 21:55:46 +0300

    'deploy'

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ dotenv.config({ path: root(".env") });
 
 module.exports = {
 	PORT: process.env.PORT || 3000,
-	MONGO_URL: process.env.MONGO_URL,
+	MONGO_URL: process.env.MONGO_URL.toString(),
 	SESSION_SECRET: process.env.SESSION_SECRET,
 	IS_PRODUCTION: process.env.NODE_ENV === "production",
 	PER_PAGE: process.env.PER_PAGE,
